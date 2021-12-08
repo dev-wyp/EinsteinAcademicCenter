0f5e907f22aac919e4fc72ece85aa5344812852a
Wai Yan Phyo <waiyanphyo@aceplussolutions.com>
2021-12-08 23:17:51 +0630

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Einstein Academic Center
         </a>
       </header>
     </div>
